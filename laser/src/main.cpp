0672c36f363d33d9b2a962c4dd557e21bde5af32
leonbl <leon.blazic@gmail.com>
2022-12-22 12:31:32 +0100

    ...

--- laser/src/main.cpp
+++ laser/src/main.cpp
@@ -10,28 +10,22 @@ void setup() {
   while (! Serial) {
     delay(1);
   }
-  
-  Serial.println("Adafruit VL53L0X test");
+
+  Serial.println("Adafruit VL53L0X test.");
   if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
     while(1);
   }
-  // power 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+  // power
+  Serial.println(F("VL53L0X API Continuous Ranging example\n\n"));
+
+  // start continuous ranging
+  lox.startRangeContinuous();
 }
 
-
 void loop() {
-  VL53L0X_RangingMeasurementData_t measure;
-    
-  Serial.print("Reading a measurement... ");
-  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
-
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
-  } else {
-    Serial.println(" out of range ");
+  if (lox.isRangeComplete()) {
+    Serial.print("Distance in mm: ");
+    Serial.println(lox.readRange());
   }
-    
-  delay(100);
 }
